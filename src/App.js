08a2d3b41646b04ff57552c8f99c0a5282901e01
ParadoxoID <asker01adig@gmail.com>
2022-08-05 16:19:42 +0300

    Вынес категории, хедер, сортировку, пицца блок в отдельные компоненты

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from 'react';
+
+import Categories from './components/Categories';
+import Header from './components/Header';
+import PizzaBlock from './components/PizzaBlock';
+import Sort from './components/Sort';
+
+import './scss/app.scss';
 
 const App = () => {
   return (
-    <div>Hello React</div>
-  )
-}
+    <div className="wrapper">
+      <Header />
+      <div className="content">
+        <div className="container">
+          <div className="content__top">
+            <Categories />
+            <Sort />
+          </div>
+          <h2 className="content__title">Все пиццы</h2>
+          <div className="content__items">
+            <PizzaBlock title="PIIIIZZZA" />
+            <PizzaBlock />
+            <PizzaBlock />
+            <PizzaBlock />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default App
+export default App;
